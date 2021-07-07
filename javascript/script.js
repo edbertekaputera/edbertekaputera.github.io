64da3c7a221c2d29fdaa4516f7a4fa2996c25df6
Edbert Ekaputera <edbertekaputera@gmail.com>
2021-07-07 14:56:00 +0700

    Portfolio Website

---
+++ javascript/script.js
@@ -0,0 +1,83 @@
+// TYPEWRITER EFFECT ON SUBTITLE
+class TypeWriter {
+	constructor(txtElement, words, wait = 3000) {
+		this.txtElement = txtElement;
+		this.words = words;
+		this.txt = ""; 
+		this.wait = parseInt(wait, 10);
+		this.type();
+		this.isDeleting = false;
+	}
+
+	type() {
+		const FullTxt = this.words;
+
+		// Check deleting or not
+		if(this.isDeleting) {
+			//Remove char
+			this.txt = FullTxt.substring(0, this.txt.length - 1);
+		} else {
+			//Add char
+			this.txt = FullTxt.substring(0, this.txt.length + 1);
+		}
+		// inserting the characters
+		this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+		
+		// Initial type speed
+		let typeSpeed = 50;
+		// Deleting speed
+		if(this.isDeleting) {
+			typeSpeed = 25;
+		}
+		// Pause after type completion
+		if (!this.isDeleting && this.txt === FullTxt) {
+			typeSpeed = this.wait;
+			this.isDeleting = true; 
+		// Pause after deleting complete
+		} else if (this.isDeleting && this.txt === '') {
+			this.isDeleting = false;
+			typeSpeed = 500;
+		}
+
+		setTimeout(() => this.type(), typeSpeed);
+	}
+}
+//SMOOTH SCROLLING EFFECT
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+// Navigation Home Click (HIGHLIGHT)
+
+// Navigation Education Click (SCROLL AND HIGHLIGHT)
+
+document.addEventListener('DOMContentLoaded', init);
+// Init on DOM load
+// Init app
+function init() {
+	const txtElement = document.querySelector('.home-subtitle');
+	const words = txtElement.getAttribute("data-words");
+	const wait = txtElement.getAttribute("data-wait");
+	new TypeWriter(txtElement, words, wait);
+}
