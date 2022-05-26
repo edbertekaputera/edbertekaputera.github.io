5360ba60238c61e561e1c38661288d6069f562f9
Edbert Ekaputera <edbertekaputera@gmail.com>
2022-05-26 16:39:10 +0800

    update

---
+++ csit114-bspoke/Membership/assets/js/index.js
@@ -0,0 +1,10 @@
+var web3;
+
+async function Connect(){
+    await window.web3.currentProvider.enable();
+    web3 = new web3(window.web3.currentProvider);
+}
+
+$('.connect-metamask').click(function(){
+    Connect();
+});
